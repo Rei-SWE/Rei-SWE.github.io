e09f9c8ede11647d6f5d31ba193fdfba75819e9f
Exia <168421674+Exia-SWE@users.noreply.github.com>
2024-08-25 15:40:12 +0900

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -16,6 +16,24 @@ const home = document.querySelector('.home_container')
 const homeHeight = home.offsetHeight;
 
 document.addEventListener('scroll', () =>{
-  console.log(1 - window.scrollY / homeHeight)
+  console.log(homeHeight / 2)
   home.style.opacity = 1 - window.scrollY / homeHeight
 })
+
+
+
+// Arrow up버튼을 아래로 스크롤시 투명하게 처리함
+
+const arrowUp = document.querySelector('.arrow-up')
+
+document.addEventListener('scroll', () => {
+  window.scrollY > homeHeight / 2 ? arrowUp.style.opacity = 1 : arrowUp.style.opacity = 0
+})
+
+
+
+  // if(window.scrollY > homeHeight / 2) {
+  //   arrowUp.style.opacity = 1
+  // } else {
+  //   arrowUp.style.opacity = 0
+  // }
